658f1fa25780fe33eb568ffece70f7d88e61a70c
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-16 00:54:48 -0400

    Un-Add js file--did not seem to change anything

--- assets/responsive-grid.js
+++ assets/responsive-grid.js
@@ -1,8 +1,11 @@
 
-
-$('.grid-tile').on('hover', function(){
-  $('.grid-tile-icon, .grid-tile-title').addClass('showing');
-  $('.grid-tile-desc').css('display', 'block');
-});
-
-console.log('hover thing works');
+// 
+// $('.grid-tile').on('hover', function(){
+//   $('.grid-tile-icon, .grid-tile-title').addClass('showing');
+// });
+//
+// $('.grid-tile').on('hover', function(){
+//     $('.grid-tile-desc').css('display', 'block');
+// });
+//
+// console.log('hover thing works');
