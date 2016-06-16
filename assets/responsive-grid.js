965bcf6155277533c9284a7d223de21ef8e86cd0
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-16 00:26:19 -0400

    Add JS file

---
+++ assets/responsive-grid.js
@@ -0,0 +1,8 @@
+
+
+$('.grid-tile').on('hover', function(){
+  $('.grid-tile-icon, .grid-tile-title').addClass('showing');
+  $('.grid-tile-desc').css('display', 'block');
+});
+
+console.log('hover thing works');
